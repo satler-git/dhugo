04955a4d3b3178f0850c1e5176277e80359989de
satler-git <mitsclatw24@mbr.nifty.com>
2023-12-06 11:02:56 +0000

    Add sample test  #5

---
+++ renderer/__test__/test.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+	isActive: true,
+	age: 32,
+};
+
+describe("person", () => {
+	test("person is defined", () => {
+		expect(person).toBeDefined();
+	});
+
+	test("is active", () => {
+		expect(person.isActive).toBeTruthy();
+	});
+
+	test("age limit", () => {
+		expect(person.age).toBeLessThanOrEqual(32);
+	});
+});
